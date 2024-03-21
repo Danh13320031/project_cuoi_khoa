5840ea7139db9a65fc18c12874aefa81b1913799
Danh13320031 <danh13320031@gmail.com>
2024-03-21 23:24:50 +0700

    Render product data to UI

--- configs/mongDB.config.js
+++ configs/mongDB.config.js
@@ -1,12 +1,12 @@
-import mongoose from "mongoose";
-import env from "./environment.config.js";
+import mongoose from 'mongoose';
+import env from './environment.config.js';
 
 const connect = async () => {
   try {
     await mongoose.connect(env.databaseUrl);
-    console.log("Connect Success!");
+    console.log('Connect Database Success!');
   } catch (error) {
-    console.log("Connect Error: ", error);
+    console.log('Connect Database Error: ', error);
   }
 };
 
--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,17 +1,7 @@
-import productModel from '../../models/admin/product.model.js';
-
-const index = async (request, response, next) => {
-  try {
-    const productListData = await productModel.find({});
-
-    response.status(200);
-    response.render('admin/pages/product/product.view.ejs', {
-      pageTitle: 'Trang quản lý sản phẩm',
-      productListData,
-    });
-  } catch (error) {
-    console.log(error);
-  }
+const index = (request, response, next) => {
+  response.render('admin/pages/product/product.view.ejs', {
+    pageTitle: 'Trang quản lý sản phẩm',
+  });
 };
 
 const productController = {
